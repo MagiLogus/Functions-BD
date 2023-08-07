6615eca58641ea6e51198f93d9d3d41fda4bca7f
Paulo Gonaçlves <paulo.oliveira.phgo@gmail.com>
2023-08-07 10:41:31 -0300

    .

--- tabelas.sql
+++ tabelas.sql
@@ -1,3 +1,21 @@
--- Demonstrando a função STRING_SPLIT para dividir uma string em uma tabela.
--- Suponha que você tenha uma string 'Richard,Fernandes,Passarelli' e queira dividi-la em cada vírgula.
-SELECT value FROM STRING_SPLIT('Richard,Fernandes,Passarelli', ',');
+-- Criar a tabela Pessoas
+CREATE TABLE Pessoas (
+    ID INT PRIMARY KEY,
+    NomeCompleto VARCHAR(100)
+);
+
+-- Inserir alguns registros na tabela Pessoas
+INSERT INTO Pessoas (ID, NomeCompleto)
+VALUES
+    (1, 'Richard,Fernandes,Passarelli'),
+    (2, 'João,Silva,Santos'),
+    (3, 'Maria,Aparecida,Rodrigues');
+
+-- Selecionar os nomes divididos usando a função STRING_SPLIT
+SELECT
+    ID,
+    Value AS PrimeiroNome,
+    LEAD(Value) OVER (ORDER BY (SELECT NULL)) AS SegundoNome,
+    LEAD(Value, 2) OVER (ORDER BY (SELECT NULL)) AS Sobrenome
+FROM Pessoas
+CROSS APPLY STRING_SPLIT(NomeCompleto, ',');
